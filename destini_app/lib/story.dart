bde96e2ca7f7ec138df7a460b4a73a26d1c921ab
Rakun12 <fiqreramadhan12@gmail.com>
2024-06-11 00:15:26 +0700

    destini app

---
+++ destini_app/lib/story.dart
@@ -0,0 +1,9 @@
+class Story {
+  // membuat 3 properties
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  // membuat constructor
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+}
--- destinied_app/android/app/src/main/kotlin/com/example/destinied_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.destinied_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
