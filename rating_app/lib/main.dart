a9beb3df56863c3786f5cb87c089a96eb89f4cd1
Rakun12 <fiqreramadhan12@gmail.com>
2024-06-29 19:10:14 +0700

    Adding rating app

---
+++ rating_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:rating_bar/page_try.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rating App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RatingScreen(),
+    );
+  }
+}
